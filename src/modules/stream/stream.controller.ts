46169189f0f76c22efca9d2ed50b2af2820e69a6
A.Roshchektaev <a.roshchektaev@corusconsulting.ru>
2022-11-16 00:10:25 +0300

    first commit

---
+++ src/modules/stream/stream.controller.ts
@@ -0,0 +1,48 @@
+import {Router} from  'express'
+import WebTorrent from 'webtorrent'
+
+const router = Router()
+const client = new WebTorrent()
+
+let state = {
+    progress:0,
+    downloadSpeed:0,
+    ratio:0
+}
+
+let error
+
+client.on('error', (err: Error) => {
+    console.error(err.message)
+    error = err.message
+})
+
+client.on('torrent', () => {
+    state = {
+        progress: Math.round(client.progress * 100 * 100) / 100,
+        downloadSpeed: client.downloadSpeed,
+        ratio: client.ratio
+    }
+})
+
+router.get('/add/:magnet',(req, res)=> {
+    const magnet = req.params.magnet
+    client.add(magnet, (torrent) => {
+        const files = torrent.files.map(elem =>({
+            name:elem.name,
+            length: elem.length
+        }))
+        res.status(200).send(files)
+    })
+})
+
+router.get('/stats',(req, res) => {
+    state = {
+        progress: Math.round(client.progress * 100 * 100) / 100,
+        downloadSpeed: client.downloadSpeed,
+        ratio: client.ratio
+    }
+    res.status(200).send(state)
+})
+
+export default router
